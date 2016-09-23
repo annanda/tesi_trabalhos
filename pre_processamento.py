04fe6fb379f1e72fa65a764feecdd8f4539a97a5
Annanda Sousa <annanda.sousa@gmail.com>
2016-09-22 22:53:42 -0300

    consegui tirar a parte final indesejada do artigo e tirar os Edit dos subtitulos

--- pre_processamento.py
+++ pre_processamento.py
@@ -3,25 +3,26 @@ import re
 
 class ArticleReader:
     title = None
+    texto = None
     conteudo = []
 
     def __init__(self, from_path):
         with open(from_path, 'r') as f:
-            text_file = f.readlines()
-            # self.extrai_titulo(text_file)
-            # self.corta_image_galerry(text_file)
-            self.corta_edit(text_file)
+            text_lines = f.readlines()
+            text_file = " ".join(text_lines)
+            self.extrai_titulo(text_file)
+            self.corta_edit(text_lines)
+            self.corta_image_galerry()
 
+    def extrai_titulo(self, texto):
+        regex_1 = re.compile(r'"(.+)"')
+        list = re.findall(regex_1, texto)
+        self.title = list[0]
 
-    # def extrai_titulo(self, texto):
-    #     regex_1 = re.compile(r'"(.+)"')
-    #     list = re.findall(regex_1, texto)
-    #     self.title = list[0]
-    #
-    # def corta_image_galerry(self, texto):
-    #     regex_2 = re.compile(r'(".*)Ima', flags=re.DOTALL)
-    #     list_2 = re.findall(regex_2, texto)
-    #     self.conteudo = list_2[0]
+    def corta_image_galerry(self):
+        regex_2 = re.compile(r'(".*)Ima', flags=re.DOTALL)
+        list_2 = re.findall(regex_2, self.texto)
+        self.texto = list_2[0]
 
     def corta_edit(self, text_file):
         for line in text_file:
@@ -34,16 +35,12 @@ class ArticleReader:
                 line = re.sub(regex_3, list_2[0], line)
             if line != '\n':
                 self.conteudo.append(line)
-        print(self.conteudo)
+        self.texto = " ".join(self.conteudo)
 
     def save_articles(self, to):
         with open(to + "/" + self.title + ".txt", 'w') as article_file:
-            article_file.write(self.conteudo)
-
+            article_file.write(self.texto)
 
 
 article = ArticleReader("raw_documents/season_1/a_golden_crown.txt")
-# article.save_articles("documents")
-# print(article.conteudo)
-# print(article.title)
-# print(len(article.conteudo))
+article.save_articles("documents")
