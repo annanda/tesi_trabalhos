b70ee4ea62d528a74cb86e6182dcab087607d50a
Annanda Sousa <annanda.sousa@gmail.com>
2016-10-04 22:38:44 -0300

    Consertando erro que dava por atributos de objetos. Lendo e processando todos os arquivos das pastas de seasons.

--- pre_processamento.py
+++ pre_processamento.py
@@ -1,12 +1,14 @@
 import re
+import glob
 
 
 class ArticleReader:
-    title = None
-    texto = None
-    conteudo = []
 
     def __init__(self, from_path):
+        self.title = None
+        self.texto = None
+        self.texto_final = None
+        self.conteudo = []
         with open(from_path, 'r') as f:
             text_lines = f.readlines()
             text_file = " ".join(text_lines)
@@ -21,11 +23,11 @@ class ArticleReader:
 
     def corta_image_galerry(self):
         regex_2 = re.compile(r'(".*)Ima', flags=re.DOTALL)
-        list_2 = re.findall(regex_2, self.texto)
-        self.texto = list_2[0]
+        list_2 = re.findall(regex_2, self.texto_final)
+        self.texto_final = list_2[0]
 
-    def corta_edit(self, text_file):
-        for line in text_file:
+    def corta_edit(self, text_lines):
+        for line in text_lines:
             regex_3 = re.compile(r'^.*Edit')
             regex_4 = re.compile(r'(^.*)Edit')
             list_1 = re.findall(regex_3, line)
@@ -35,12 +37,29 @@ class ArticleReader:
                 line = re.sub(regex_3, list_2[0], line)
             if line != '\n':
                 self.conteudo.append(line)
-        self.texto = " ".join(self.conteudo)
+        self.texto_final = " ".join(self.conteudo)
 
     def save_articles(self, to):
         with open(to + "/" + self.title + ".txt", 'w') as article_file:
-            article_file.write(self.texto)
+            article_file.write(self.texto_final)
 
 
-article = ArticleReader("raw_documents/season_1/a_golden_crown.txt")
-article.save_articles("documents")
+
+
+all_seasons_files = []
+
+for season in glob.glob("raw_documents/*"):
+    all_seasons_files.append(season)
+
+for season in all_seasons_files:
+    file_list = glob.glob(season + "/*.txt")
+    # print(file_list)
+    regex = re.compile(r'raw_documents\/(season_[1-6])')
+    season_number = re.findall(regex, season)
+    for episode in file_list:
+        article = ArticleReader(episode)
+        article.save_articles("documents/"+ season_number[0])
+
+
+
+
