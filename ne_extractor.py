17e72d6ad680bd49647b7daccb5ebae794cd58d2
Annanda Sousa <annanda.sousa@gmail.com>
2016-10-22 14:55:40 -0200

    Primeira tentativa de obter entidades nomeadas, apenas pegando as palavras NN e NNP que começam com letras maiúsculas.

--- ne_extractor.py
+++ ne_extractor.py
@@ -4,6 +4,7 @@ import nltk
 class NEExtractor:
     def __init__(self, article_path):
         self.read_article(article_path)
+        self.named_entities = []
 
     def nominated_entities(self):
 
@@ -17,6 +18,17 @@ class NEExtractor:
         with open(path, 'r') as article:
             self.article = article.read()
 
+    def get_ne(self, tagged_sentences):
+        for sentence in tagged_sentences:
+            for word in sentence:
+                if word[1] == 'NN' or word[1] == 'NNP':
+                    if self.is_first_letter_upper(word[0]):
+                        self.named_entities.append(word[0])
+
+    def is_first_letter_upper(self, word):
+        return word[0].isupper()
 
 extractor = NEExtractor("documents/season_1/Baelor.txt")
-print(extractor.nominated_entities())
+extractor.get_ne(extractor.nominated_entities())
+print(extractor.named_entities)
+print(len(extractor.named_entities))
