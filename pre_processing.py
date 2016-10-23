110991a8f23c45accc9c012f86ebd71e7370cca1
Annanda Sousa <annanda.sousa@gmail.com>
2016-10-23 15:20:08 -0200

    Pegando as seções Plot e Summary.

--- pre_processing.py
+++ pre_processing.py
@@ -6,13 +6,16 @@ class ArticleReader:
 
     def __init__(self, from_path):
         self.title = None
-        self.texto = None
+        self.plot = None
+        self.summary = None
         self.texto_final = None
-        self.conteudo = []
         with open(from_path, 'r') as f:
             text_lines = f.readlines()
             text_file = " ".join(text_lines)
             self.extrai_titulo(text_file)
+            self.get_plot(text_file)
+            self.get_summary(text_file)
+            # self.save_articles("second_processing")
 
     def extrai_titulo(self, texto):
         regex_1 = re.compile(r'"(.+)"')
@@ -26,6 +29,21 @@ class ArticleReader:
         episode_number = episode_number[0]
 
         self.title = self.title + " s" + season_number + "e" + episode_number
+
+    def get_plot(self, texto):
+        regex_1 = re.compile(r'Plot(.*)Summary', flags=re.DOTALL)
+        list = re.findall(regex_1, texto)
+        self.plot = list[0]
+
+    def get_summary(self, texto):
+        regex_1 = re.compile(r'Summary(.*)Appearances\n\s Main', flags=re.DOTALL)
+        list = re.findall(regex_1, texto)
+        self.summary = list[0]
+
+    def monta_texto_final(self):
+        self.texto_final = self.title
+        self.texto_final += self.plot
+        self.texto_final += self.summary
 
     # def save_articles(self, to):
     #     with open(to + "/" + self.title + ".txt", 'w') as article_file:
@@ -46,4 +64,6 @@ class ArticleReader:
 #         article.save_articles("documents/" + season_number[0])
 
 article = ArticleReader("documents/season_1/Baelor.txt")
-print(article.title)
+# print(article.title)
+# print(article.plot)
+print(article.summary)
