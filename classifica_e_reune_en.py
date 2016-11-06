fa77432a930ef5dc0cbd08cc81d826ebad66b2f2
daniellima <daniellima.pessoal@gmail.com>
2016-11-06 18:13:22 -0200

    Melhora no classificador para achar lugares e criação do script que marca o texto com as entidades

--- classifica_e_reune_en.py
+++ classifica_e_reune_en.py
@@ -1,52 +1,38 @@
-class EN:
-    def __init__(self):
-        self._original = None
-        self.owords = []
-        self._canonico = None
-        self.cwords = []
-        self.classification = None
+from collections import defaultdict
 
-    @property
-    def canonico(self):
-        return self._canonico
-
-    @canonico.setter
-    def canonico(self, value):
-        self._canonico = value
-
-        if value is not None:
-            self.cwords = value.split(" ")
-        else:
-            self.cwords = []
-
-    @property
-    def original(self):
-        return self._original
-
-    @original.setter
-    def original(self, value):
-        self._original = value
-
-        if value is not None:
-            self.owords = value.split(" ")
-        else:
-            self.owords = []
-
-    def __str__(self):
-        return "{0} ({1}) - {2}".format(self.canonico, self.original, self.classification)
+import jsonpickle
+from ne import EN
 
 
-with open("entidades_nomeadas.csv", 'r') as file:
-    lines = file.readlines()
-    lines = map(lambda l: l.strip(), lines)
+# with open("entidades_nomeadas.csv", 'r') as file:
+#     lines = file.readlines()
+#     lines = map(lambda l: l.strip(), lines)
+#
+# entidades = []
+# for line in lines:
+#     entidade = EN()
+#     entidade.original = line
+#     entidade.canonico = entidade.original
+#     entidade.classification = "other"
+#     entidades.append(entidade)
 
-entidades = []
-for line in lines:
-    entidade = EN()
-    entidade.original = line
-    entidade.canonico = entidade.original
-    entidade.classification = "other"
-    entidades.append(entidade)
+
+class Ocurrence:
+    def __init__(self, index, sentence):
+        self.index = index
+        self.sentence = sentence
+
+
+with open("entidades_nomeadas.json", 'r') as file:
+    entidades = jsonpickle.decode(file.read())
+
+ne_ocurrences = {}
+for entidade in entidades:
+    if entidade.original not in ne_ocurrences:
+        ne_ocurrences[entidade.original] = []
+    ne_ocurrences[entidade.original].append(Ocurrence(entidade.start_index, entidade.sentence))
+
+entidades = sorted(list(set(entidades)))
 
 for entidade in entidades:
 
@@ -120,24 +106,95 @@ for entidade in entidades:
         entidade.classification = "person"
         entidade.canonico = names_to_full_names[entidade.cwords[0]]
 
+for entidade in entidades:
+
+    if entidade.classification == "other":
+        sounds_like_a_place = 0
+        for ocurrence in ne_ocurrences[entidade.original]:
+            if ocurrence.index > 2 \
+                    and ocurrence.sentence[ocurrence.index - 3] == "return" \
+                    and ocurrence.sentence[ocurrence.index - 2] == "home" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "to":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "in" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "the":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "visit" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "to":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "return" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "to":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "out" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "of":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "back" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "to":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "at" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "the":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "return" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "from":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "arrive" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "at":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "from" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "the":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "leave" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "for":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 1 \
+                    and ocurrence.sentence[ocurrence.index - 2] == "to" \
+                    and ocurrence.sentence[ocurrence.index - 1] == "the":
+                sounds_like_a_place += 1
+            elif ocurrence.index > 0 and ocurrence.sentence[ocurrence.index - 1] in ["at", "in", "depart", "on", "to", "near", "reaches", "outside", "from"]:
+                sounds_like_a_place += 1
+
+        if sounds_like_a_place > 0:
+            print("{0} {1}/{2} ({3}%)".format(entidade, sounds_like_a_place, len(ne_ocurrences[entidade.original]), sounds_like_a_place/len(ne_ocurrences[entidade.original])*100))
+
+        if sounds_like_a_place > 5:
+            entidade.classification = "place"
+
 with open("classifica_e_reune10.csv", 'w+') as file:
     for entidade in entidades:
         file.write(str(entidade))
         file.write("\n")
 
-for entidade in entidades:
-    print(entidade)
+# with open("classifica_e_reune.json", 'w+') as file:
+#     file.write(jsonpickle.encode(entidades))
+
+# for entidade in entidades:
+#     print(entidade)
+
 print("\n----------------\n")
+
 quantas_entidades = len(entidades)
 quantas_entidades_pos_processamento = len(set([en.canonico for en in entidades]))
 quantas_entidades_com_other = len([en for en in entidades if en.classification == "other"])
 quantas_entidades_com_person = len([en for en in entidades if en.classification == "person"])
+quantas_entidades_com_place = len([en for en in entidades if en.classification == "place"])
 
 print("De {0} entidades para {1} entidades.".format(quantas_entidades, quantas_entidades_pos_processamento))
 print("Entidades com other: {0} ({1})".format(quantas_entidades_com_other,
                                               quantas_entidades_com_other / quantas_entidades * 100))
 print("Entidades com person: {0} ({1})".format(quantas_entidades_com_person,
                                                quantas_entidades_com_person / quantas_entidades * 100))
+print("Entidades com place: {0} ({1})".format(quantas_entidades_com_place,
+                                              quantas_entidades_com_place / quantas_entidades * 100))
 
 # Titulos:
 # Remover os titulos do nome canonico
@@ -151,10 +208,23 @@ print("Entidades com person: {0} ({1})".format(quantas_entidades_com_person,
 # King [DONE]
 # Queen [DONE]
 # Queen Regent [DONE]
+# Meister
+# Grand Meister
+
+# Tirar ' no final do nomes
 
 # O que fazer com ens que são "Lady", "Lord Commander", "Lord 's Choosen", etc...
 
 # Palavra The:
 # Nada. Aparentemente, tudo que começa com The não se refere a mais nada
+
+# Prefixos de lugares
+# at
+# depart
+# in
+# out of
+# in the
+# at the
+# back to
 
 # Palavra "Houses"
---
+++ marca_texto.py
@@ -0,0 +1,40 @@
+import jsonpickle
+import glob
+import os
+
+
+def tag_text(text, entities):
+    sorted_entities = sorted(entities, key=lambda it: len(it.original), reverse=True)
+
+    for i, entity in enumerate(sorted_entities):
+        text = text.replace(entity.original, "<<{0}>>".format(i))
+
+    for i, entity in enumerate(sorted_entities):
+        text = text.replace("<<{0}>>".format(i),
+                            "<en name='{0}' type='{1}'>{2}</en>".format(entity.canonico,
+                                                                        entity.classification,
+                                                                        entity.original))
+
+    return text
+
+
+with open("classifica_e_reune.json", 'r') as file:
+    entities = jsonpickle.decode(file.read())
+
+seasons_files = glob.glob("pre_processed_documents/*")
+for season in seasons_files:
+    season_name = os.path.basename(season)
+
+    episodes_files = glob.glob(season + "/*.txt")
+    for episode_file in episodes_files:
+        episode_name = os.path.basename(episode_file)
+        save_dir = os.path.join("tagged_documents", season_name)
+        os.makedirs(save_dir, exist_ok=True)
+
+        with open(episode_file, 'r', encoding='utf-8') as input:
+            episode_text = input.read()
+
+        tagged_text = tag_text(episode_text, entities)
+
+        with open(os.path.join(save_dir, episode_name), 'w+', encoding='utf-8') as output:
+            output.write(tagged_text)
